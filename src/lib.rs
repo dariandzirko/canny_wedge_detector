65cae1a0a4c1c50102d6927609d895e75bf2015f
Darian <dariandzirko@gmail.com>
2024-02-28 15:55:57 -0700

    Cleaned up code into a function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,42 @@
-use image::{imageops::blur, ImageBuffer, Rgba};
-use num::traits::float;
+use image::{DynamicImage, ImageBuffer, Rgba};
 use wasm_bindgen::{prelude::*, Clamped};
 use web_sys::*;
 
 use oxidized_image_processing::{self, float_image, helper_ops::conv_2d, kernel::Kernel};
+
+pub fn put_image_to_canvas(
+    image: &DynamicImage,
+    context: &CanvasRenderingContext2d,
+    dx: f64,
+    dy: f64,
+    rgba: bool,
+) {
+    let image_data = if rgba {
+        let image_buffer = image.to_rgba8();
+        ImageData::new_with_u8_clamped_array_and_sh(
+            Clamped(image_buffer.as_raw()),
+            image.width(),
+            image.height(),
+        )
+        .unwrap()
+    } else {
+        let image_buffer = image.to_luma8();
+        let image_buffer_raw = image_buffer.as_raw();
+        let rgba_image: Vec<u8> = image_buffer_raw
+            .into_iter()
+            .map(|pix| [*pix, *pix, *pix, 255])
+            .flatten()
+            .collect();
+        ImageData::new_with_u8_clamped_array_and_sh(
+            Clamped(rgba_image.as_slice()),
+            image.width(),
+            image.height(),
+        )
+        .unwrap()
+    };
+
+    context.put_image_data(&image_data, dx, dy).unwrap();
+}
 
 #[wasm_bindgen(start)]
 fn start() -> Result<(), JsValue> {
@@ -30,35 +63,13 @@ fn start() -> Result<(), JsValue> {
     ));
 
     let image = image::load_from_memory_with_format(&IMAGE_BYTES, image::ImageFormat::Png).unwrap();
-    let rgba_image = image.as_rgba8().unwrap();
-    let rgba_image_data = ImageData::new_with_u8_clamped_array_and_sh(
-        Clamped(rgba_image.as_raw()),
-        image.width(),
-        image.height(),
-    )
-    .unwrap();
-
-    let binding = image.to_luma8();
 
     //Before blur
-    let luma_img = binding.as_raw();
-
-    let rgba_luma_image: Vec<u8> = luma_img
-        .into_iter()
-        .map(|pix| [*pix, *pix, *pix, 255])
-        .flatten()
-        .collect();
-
-    let luma_image_data = ImageData::new_with_u8_clamped_array_and_sh(
-        Clamped(rgba_luma_image.as_slice()),
-        image.width(),
-        image.height(),
-    )
-    .unwrap();
-
-    context.put_image_data(&luma_image_data, 0.0, 0.0)?;
+    put_image_to_canvas(&image, &context, 0.0, 0.0, true);
+    // put_image_to_canvas(&image, &context, image.width() as f64, 0.0, false);
 
     //After blur
+    let binding = image.to_luma8();
     let mut blurred_float_image = float_image::FloatImage::from_luma8(binding);
     blurred_float_image.matrix = conv_2d(
         &mut Kernel::gaussian_2d(5.0).matrix,
@@ -67,22 +78,8 @@ fn start() -> Result<(), JsValue> {
     );
 
     let blurred_binding = blurred_float_image.to_luma8();
-    let blurred_luma_image = blurred_binding.as_raw();
-
-    let blurred_rgba_luma_image: Vec<u8> = blurred_luma_image
-        .into_iter()
-        .map(|pix| [*pix, *pix, *pix, 255])
-        .flatten()
-        .collect();
-
-    let blurred_luma_image_data = ImageData::new_with_u8_clamped_array_and_sh(
-        Clamped(blurred_rgba_luma_image.as_slice()),
-        image.width(),
-        image.height(),
-    )
-    .unwrap();
-
-    context.put_image_data(&blurred_luma_image_data, 0.0, image.height() as f64)?;
+    let dyn_image = DynamicImage::from(blurred_binding);
+    put_image_to_canvas(&dyn_image, &context, 0.0, image.height() as f64, true);
 
     Ok(())
 }
